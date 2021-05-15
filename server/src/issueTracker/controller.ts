b572ed6ea99996b23024e264b503e0434de3d8cc
Jakub Gawryl <jacqbus15@gmail.com>
2021-05-15 15:31:06 +0200

    Implement "update issue" functionality

--- server/src/issueTracker/controller.ts
+++ server/src/issueTracker/controller.ts
@@ -8,6 +8,11 @@ export const getAllIssues = (): Issue[] => {
   return model.issues;
 }
 
+export const getIssueById = (issueId: string): Issue | null => {
+  const filtered = model.issues.filter((issue: Issue) => issue.id === issueId);
+  return filtered[0] || null;
+}
+
 export const createIssue = (title: string, description: string, status: EIssueStatus = EIssueStatus.OPEN): Issue => {
   const newIssue: Issue = {
     id: uuidv4(),
--- server/src/routes/IssuesRouter.ts
+++ server/src/routes/IssuesRouter.ts
@@ -1,6 +1,7 @@
+import {EIssueStatus} from './../issueTracker/types';
 import express from 'express'
 const router = express.Router()
-import {getAllIssues, createIssue} from '../issueTracker/controller';
+import {getAllIssues, getIssueById, createIssue} from '../issueTracker/controller';
 
 
 /**
@@ -11,6 +12,27 @@ router.get('/issues', async (req, res) => {
   res.json({
     success: true,
     data: getAllIssues()
+  });
+});
+
+/**
+* @route         GET     /issues/:issueId
+* @description           GET single issue by it's ID
+*/
+router.get('/issues/:issueId', async (req, res) => {
+  const {issueId} = req.params;
+  const issue = getIssueById(issueId);
+
+  if (!issue) {
+    return res.status(404).json({
+      success: false,
+      msg: `Issue with id ${issueId} not found!`
+    });
+  };
+
+  res.json({
+    success: true,
+    data: issue
   });
 });
 
@@ -33,6 +55,62 @@ router.post('/issues', async (req, res) => {
   res.json({
     success: true,
     data: createdIssue
+  });
+});
+
+/**
+* @route         PUT      /issues/:issueId
+* @description            Update issue
+*/
+router.put('/issues/:issueId', async (req, res) => {
+  const {issueId} = req.params
+  const {title, description, status: newStatus} = req.body;
+
+  const issue = getIssueById(issueId);
+
+  if (!issue || !issueId) {
+    return res.status(400).json({
+      success: false,
+      msg: `Issue with id ${issueId} not found!`
+    })
+  }
+
+  if (newStatus) {
+    const {OPEN, PENDING, CLOSED} = EIssueStatus;
+    
+    // Check that the status is correct
+    if (!Object.values(EIssueStatus).includes(newStatus)) {
+      return res.status(400).json({
+        success: false,
+        msg: `Incorrect status ${newStatus}!`
+      })
+    }
+
+    const statusCanBechanged = issue.status === OPEN || (issue.status === PENDING && newStatus !== OPEN) || (issue.status === CLOSED && newStatus !== OPEN && newStatus !== PENDING)
+
+    if (!statusCanBechanged) {
+      return res.status(400).json({
+        success: false,
+        msg: `Cannot change status to ${newStatus}!`
+      })
+    }
+
+    // Modify status
+    issue.status = newStatus;
+  }
+
+  // Modify the rest of the parameters, if they are sent
+  // TODO - Modification of the issue parameters should take place in the controller!!!
+  if (title) {
+    issue.title = title;
+  }
+  if (description) {
+    issue.description = description;
+  }
+
+  res.json({
+    success: true,
+    data: issue
   });
 });
 
